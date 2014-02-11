94a5faf37d3a2abbe325d1e9f9f75866e3ecbe37
qingliangcn <qing.liang.cn@gmail.com>
2014-02-11 21:57:56 +0800

    put log api macro to one single file

--- include/mslog.hrl
+++ include/mslog.hrl
@@ -8,6 +8,8 @@
 %%%-------------------------------------------------------------------
 -author("qingliang").
 
+-include("mslog_macro.hrl").
+
 %% syntax similar with ?: in c
 -ifndef(IF).
 -define(IF(C, T, F), (case (C) of true -> (T); false -> (F) end)).
@@ -35,32 +37,6 @@
 -ifdef(EUNIT).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
-
-%% 日志记录
-%% 程序日志记录宏
--define(DEV(Format, Args),
-    mslog_logger_dyn:dev(node(), ?MODULE, ?LINE, Format, Args)).
--define(DEV(D),?DEV(D, [])).
-
--define(DEBUG(Format, Args),
-    mslog_logger_dyn:debug_msg(node(), ?MODULE,?LINE,Format, Args)).
--define(DEBUG(D), ?DEBUG(D, [])).
-
--define(INFO_MSG(Format, Args),
-    mslog_logger_dyn:info_msg(node(), ?MODULE,?LINE,Format, Args)).
--define(INFO_MSG(D), ?INFO_MSG(D, [])).
-
--define(WARNING_MSG(Format, Args),
-    mslog_logger_dyn:warning_msg( node(), ?MODULE,?LINE,Format, Args)).
--define(WARNING_MSG(D), ?WARNING_MSG(D, [])).
-
--define(ERROR_MSG(Format, Args),
-    mslog_logger_dyn:error_msg( node(), ?MODULE,?LINE,Format, Args)).
--define(ERROR_MSG(D), ?ERROR_MSG(D, [])).
-
--define(CRITICAL_MSG(Format, Args),
-    mslog_logger_dyn:critical_msg( node(), ?MODULE,?LINE,Format, Args)).
--define(CRITICAL_MSG(D), ?CRITICAL_MSG(D, [])).
 
 %% 异常捕获相关
 -define(TRY_CATCH(Expression), ?TRY_CATCH(Expression,ErrType,ErrReason)).
